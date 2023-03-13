9bf2a93d1a4223051b6113c7a088629f0310f3c2
Andrey <plowand@gmail.com>
2023-03-13 21:46:15 +0200

    ccc

--- DECAPP/DECAPP.Android/Resources/Resource.designer.cs
+++ DECAPP/DECAPP.Android/Resources/Resource.designer.cs
@@ -14,7 +14,11 @@ namespace DECAPP.Droid
 {
 	
 	
+<<<<<<< Updated upstream
 	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.1.0.11")]
+=======
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.0.99.154")]
+>>>>>>> Stashed changes
 	public partial class Resource
 	{
 		
